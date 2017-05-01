108d0ba4c339aed9814575f63e5ebfb6505108b4
kailasa <kailasandhale888@gmail.com>
2017-05-02 00:47:00 +0530

    Consumer

--- src/com/kailasa/exercise/Unit1ExerciseSolutionJava8.java
+++ src/com/kailasa/exercise/Unit1ExerciseSolutionJava8.java
@@ -3,8 +3,8 @@ package com.kailasa.exercise;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 
@@ -23,16 +23,16 @@ public class Unit1ExerciseSolutionJava8 {
 		Collections.sort(people, (p1,p2) -> p1.getLastName().compareTo(p2.getLastName()));
 		
 		System.out.println("Printing all Pesons\n");
-		printConditionally(people, p -> true);
+		printConditionally(people, p -> true, p -> System.out.println(p));
 		
 		System.out.println("\nPrinting Conditionally\n");
-		printConditionally(people, (p) -> p.getLastName().startsWith("C"));
+		printConditionally(people, (p) -> p.getLastName().startsWith("C"),p -> System.out.println(p.getFirstName()));
 	}
 
-	private static void printConditionally(List<Person> people,Predicate<Person> predicate) {
+	private static void printConditionally(List<Person> people,Predicate<Person> predicate, Consumer<Person> consumer) {
 		for (Person person : people) {
 			if(predicate.test(person))
-				System.out.println(person);
+				consumer.accept(person);
 		}
 		
 	}
